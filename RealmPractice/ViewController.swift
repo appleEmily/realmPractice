115ed55934165930babdaf04cc20123e4a556792
Emily Nozaki <emiri3128.gmail.com>
2022-01-25 19:19:45 +0900

    Initial Commit

---
+++ RealmPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmPractice
+//
+//  Created by Emily Nozaki on 2022/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
